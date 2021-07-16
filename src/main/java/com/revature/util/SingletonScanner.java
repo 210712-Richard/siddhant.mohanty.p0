ceeb62b60ee5d764e0f544781ae4ad76370e1262
Sidd <siddhant_99@hotmail.com>
2021-07-15 21:49:58 -0700

    project update 2

--- src/main/java/com/revature/util/SingletonScanner.java
+++ src/main/java/com/revature/util/SingletonScanner.java
@@ -1,5 +1,24 @@
 package com.revature.util;
 
-public class SingletonScanner {
+import java.util.Scanner;
 
+public class SingletonScanner {
+	private static SingletonScanner instance;
+
+	private Scanner scan;
+
+	private SingletonScanner() {
+		scan = new Scanner(System.in);
+	}
+
+	public static synchronized SingletonScanner getScanner() {
+		if (instance == null) {
+			instance = new SingletonScanner();
+		}
+		return instance;
+	}
+
+	public Scanner getScan() {
+		return scan;
+	}
 }
